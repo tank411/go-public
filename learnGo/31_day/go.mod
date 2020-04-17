cca95bcb394bb0cd8b1b5678e781fa44d84e7b72
cuiyapeng <cuiyapeng@hikvision.com>
2020-04-17 19:15:47 +0800

    [add]增加学习文件

---
+++ learnGo/31_day/go.mod
@@ -0,0 +1,5 @@
+module go-public/learnGo/31_day
+
+go 1.13
+
+require github.com/therecipe/qt v0.0.0-20200126204426-5074eb6d8c41
---
+++ learnGo/31_day/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"os"
+
+	"github.com/therecipe/qt/gui"
+	"github.com/therecipe/qt/widgets"
+)
+
+//学习参考：https://blog.csdn.net/weixin_43968923/article/details/105001824
+
+// 下面的脚本显示了如何在屏幕中心显示窗口。
+//QtGui,QDesktopWidget类提供了用户的桌面信息,包括屏幕大小。
+
+// 控制窗口显示在屏幕中心的方法
+func center(app *widgets.QMainWindow) {
+	// 获得窗口
+	qr := app.FrameGeometry()
+
+	//  获得屏幕中心点
+	cp := widgets.NewQDesktopWidget().AvailableGeometry2(app).Center()
+	// 显示到屏幕中心
+	qr.MoveCenter(cp)
+	app.Move(qr.TopLeft())
+}
+
+func InitUi() *widgets.QMainWindow {
+	// 创建窗口
+	app := widgets.NewQMainWindow(nil, 0)
+
+	// 设置窗口的标题
+	app.SetWindowTitle("Qt 教程")
+
+	// 设置窗口的位置和大小
+	app.SetGeometry2(300, 300, 300, 220)
+
+	// 设置窗口的图标，引用当前目录下的web.png图片
+	app.SetWindowIcon(gui.NewQIcon5("images/app.ico"))
+
+	// 控制窗口显示在屏幕中心的方法
+	center(app)
+
+	return app
+}
+
+func main() {
+	// 创建一个应用程序对象
+	// sys.argv参数是一个列表，从命令行输入参数
+	widgets.NewQApplication(len(os.Args), os.Args)
+
+	// 初始化窗口
+	app := InitUi()
+
+	// 显示组件
+	app.Show()
+
+	// 确保应用程序干净的退出
+	widgets.QApplication_Exec()
+}
